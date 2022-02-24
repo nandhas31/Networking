52524baaea59e1daf896dce3d7f8d6ed29293b01
GtihubGatech <nsundara3@gatech.edu>
2022-02-23 19:24:39 -0500

    complete PA1

--- PA1/client.py
+++ PA1/client.py
@@ -1,33 +1,47 @@
+# CS 3251 PA1 - Nandha and Nithik
+
 import socket
 import threading
 import sys 
+import os
 import argparse
 import atexit
 import time
 
-#TODO: Implement all code for your server here
+# TODO: Implement all code for your server here
+# Use sys.stdout.flush() after print statemtents
 PORT = None
 HOST = '127.0.0.1'
 s_global = None
-# Use sys.stdout.flush() after print statemtents
+username = "DEFAULT_USERNAME"
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Join a chat server on a specified port")
+	parser.add_argument('-join', action = 'store_true')
+	parser.add_argument('-host', dest = 'host', required=True)
 	parser.add_argument('-port', dest = 'port', required=True, type=int)
 	parser.add_argument('-username', dest = 'username', required=True)
-	parser.add_argument('-password', dest = 'password', required=True)
-	parser.add_argument('-join', action = 'store_true')
-	password, PORT, join = parser.parse_args().password, parser.parse_args().port,parser.parse_args().join
+	parser.add_argument('-passcode', dest = 'passcode', required=True)
+
+	username, HOST = parser.parse_args().username, parser.parse_args().host
+	password, PORT, join = parser.parse_args().passcode, parser.parse_args().port,parser.parse_args().join
+	if len(username) > 8:
+		print("Enter a valid username")
+		os._exit(0)
 
 def recieve_messages(s):
 	while(True):
-		time.sleep(5)
-		print(s)		
+		data = s.recv(1024).decode()
+		if not data:
+			# print("You are no longer in the chatroom")
+			os._exit(0)
+		print(data)		
+		sys.stdout.flush()
 
 def write_messages(s):
 	while(True):
-		time.sleep(5)
-		s.sendall(b'hello')		
+		message = input()
+		s.sendall(message.encode())		
 
 def close_socket():
 	s_global.close()
@@ -37,9 +51,11 @@ atexit.register(close_socket)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s_global = s
 s.connect((HOST, PORT))
-print(s)
+
+
+s.sendall((username + ":" + password).encode())
+
 reader = threading.Thread(target=recieve_messages, args=(s,))
 reader.start()
-writer= threading.Thread(target=write_messages, args=(s,))
+writer = threading.Thread(target=write_messages, args=(s,))
 writer.start()
-
--- PA1/server.py
+++ PA1/server.py
@@ -1,53 +1,93 @@
+# CS 3251 PA1 - Nandha and Nithik
+
+from calendar import week
 import socket
 import threading
 import sys 
+import os
 import argparse
-import time
+import datetime
 import traceback
 
 
 #TODO: Implement a client that connects to your server to chat with other clients here
+# Use sys.stdout.flush() after print statemtents
 start = None
 PORT = None
 password = None
 HOST = ''
 
 
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Start a chat server on a specified port")
+	parser.add_argument('-start', action = 'store_true')
+	parser.add_argument('-port', dest = 'port', required=True, type=int)
+	parser.add_argument('-passcode', dest = 'passcode', required=True)
+	password, PORT, start = parser.parse_args().passcode, parser.parse_args().port,parser.parse_args().start
+	if len(password) > 5 or not password.isalnum():
+		print("Enter a valid password")
+		sys.exit(0)
 
-# Use sys.stdout.flush() after print statemtents
 
+def time(hours):
+	t = datetime.datetime.now() + datetime.timedelta(hours=hours)
+	weekday = t.isoweekday()
+	month = t.month
+	day = t.day
+	year = t.year
+	time = str(t.time()).split(".")[0]
+
+	weekdays = {1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat", 7: "Sun"}
+	months = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
+	d = weekdays[weekday] + " " + months[month] + " " + str(day) + " " + str(time) + " " + str(year)
+	return d
+
+
+connections = {}
+t = time(0)
+shortcuts = {":)": "[feeling happy]", ":(": "[feeling sad]", ":mytime": str(t), ":+1hr": str(time(1))}
 def handle_connection(conn):
-	username = 'DEFAULT_USERNAME'
+	username, userpass = conn.recv(1024).decode().split(":")
+	if password != userpass:
+		conn.sendall("Incorrect passcode".encode())
+		conn.close()
+		return
+	conn.sendall('Connected to {} on port {}'.format('127.0.0.1', PORT).encode())
+	connections[username] = conn
 	print('{} joined the chatroom'.format(username))
+	sys.stdout.flush()
+	for user in connections:
+		if user != username:
+			connections[user].sendall("{} joined the chatroom".format(username).encode())
 	sys.stdout.flush()
 	while True:
 		try:
-			conn.sendall(b'')
-			data = conn.recv(1024)
-			if data: 
-				print(data, conn)
-				sys.stdout.flush()
-			else:
+			# conn.sendall(b'')
+			data = conn.recv(1024).decode()
+			if not data or data == ':Exit':
 				print("{} left the chatroom".format(username))
+				for user in connections:
+					if user != username:
+						connections[user].sendall("{} left the chatroom".format(username).encode())
 				sys.stdout.flush()
+				del connections[username]
 				conn.close()
-				break
+				return
+			else: 
+				if data in shortcuts:
+					data = shortcuts[data]
+				print(username + ": " + data)
+				sys.stdout.flush()
+				for user in connections:
+					if user != username:
+						connections[user].sendall((username + ": " + data).encode())
 		except:
 			traceback.print_exc()
 			break 
 
 
-if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description="Start a chat server on a specified port")
-	parser.add_argument('-port', dest = 'port', required=True, type=int)
-	parser.add_argument('-password', dest = 'password', required=True)
-	parser.add_argument('-start', action = 'store_true')
-	password, PORT, start = parser.parse_args().password, parser.parse_args().port,parser.parse_args().start
-
-
 if start:
 	print("Server started on port {}. Accepting connections".format(PORT))
-
 	sys.stdout.flush()
 
 	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -57,3 +97,5 @@ if start:
 		conn, addr = serverSocket.accept()
 		thread = threading.Thread(target=handle_connection, args=(conn,))
 		thread.start()
+
+
